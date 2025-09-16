98d0f3b043d99666ea460769eef6a0e8af9e8801
DeobratJha <deobratjha1998@gmail.com>
2025-09-16 10:57:17 +0530

    Initial commit

---
+++ app/chains.py
@@ -0,0 +1,60 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Arjun, a second year Mtech(R) student at National Insitute of Technology, Karnataka in Information Technology Department. 
+            Arjun is very dedicated and has strong computer science fundaamentals.
+            Arjun also has particpated in various AI hackathons like IndoML hackathon and National level India AI cyberguard hackathon,
+            with experience in NLP and Deep Learning and computer science fundamentals.
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of Arjun 
+            in fulfilling their needs.
+            Also add the most relevant ones from the following links to showcase Arjun's portfolio: {link_list}
+            Remember you are Arjun, Mtech(R) student at NITK. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
